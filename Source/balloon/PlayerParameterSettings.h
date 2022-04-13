a2c628070de2a5017a5844a5b37b2ee38453d335
B <pchub@pasuta.com>
2022-04-13 17:50:24 +0900

    キャラ切り替え作成中

--- Source/balloon/PlayerParameterSettings.h
+++ Source/balloon/PlayerParameterSettings.h
@@ -15,16 +15,16 @@ struct BALLOON_API FPlayerParameterSettings
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int playerType;
+	int playerType; // 何番を見ているのかを判りやすくするためだけのやつ（未使用）
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool hasStickLR_Move;
+	bool stickLR_Move; // 左スティックで左右移動を行えるかどうか
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool hasStickUD_Move;
+	bool stickUD_Move; // 左スティックで上下移動を行えるかどうか
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float jumpPower; // 0でジャンプ無効
+	float jumpPower; // ジャンプ力/自動上昇速度。0でジャンプ無効
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float jumpOK_Velocity; // 縦方向の移動量がこれ以下だったら空中ジャンプが可能
@@ -39,6 +39,9 @@ struct BALLOON_API FPlayerParameterSettings
 	bool autoRise; // 自動上昇するかどうか
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float breakLR; // 横方向の減速度 Breaking Deceleration Falling
+	float riseBreak; // 自動上昇のブレーキ力
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float breakLR; // 横方向の自動減速力
 
 };
